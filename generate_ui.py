9498a06eb7d8c89ffa03d85f411250626ad3b7cb
Caio César Viel <caioviel@gmail.com>
2013-11-24 19:27:22 -0200

    Novas interfaces

---
+++ generate_ui.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import shutil
+import os
+
+DIRECTORY = "ui"
+
+
+if __name__ == "__main__":
+    all_files = os.listdir(DIRECTORY)
+    for mfile in all_files:
+        if ".ui" in 
+            print mfile
---
+++ test/audio.py
@@ -0,0 +1,3 @@
+from PyQt4 import phonon, QtCore, QtGui, QtMultimedia
+
+Qt
