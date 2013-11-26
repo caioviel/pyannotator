0412f1a818b149bdf5f5b72a43c2a4684f54b6b7
Caio César Viel <caioviel@gmail.com>
2013-11-26 15:46:02 -0200

    ShowContent

---
+++ widget/ShowContent.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from ui.ui_ShowContent import Ui_ShowContent
+from PyQt4 import QtGui, QtCore
+import model
+import os
+HOME_DIRECTORY = os.getenv('USERPROFILE') or os.getenv('HOME')
+from LayoutSelector import *
+
+class ShowContent(QtGui.QDialog):
+    def __init__(self, parent=None):
+        super(ShowContent, self).__init__(parent)
+        self.ui = Ui_ShowContent()
+        self.ui.setupUi(self)
+        
+        self.init_ui()
+        
+    def init_ui(self):
+        self.ui.radio_info.setChecked(True)
+        self.ui.radio_tr.setChecked(True)
+        self.ui.btn_choose_icon.setEnabled(False)
+        
+        self.ui.radio_personalized.toggled.connect(self.personalized_choosed)
+        self.ui.btn_choose_icon.clicked.connect(self.choose_icon)
+        
+        self.lbl_screen = QtGui.QLabel(self.ui.widget_icon)
+        self.lbl_screen.resize(self.ui.widget_icon.size())
+        self.lbl_screen.setStyleSheet('background-color: rgb(0, 0, 0); \
+                                        border-color: rgb(255, 255, 255);\
+                                        font: 75 28pt "Ubuntu";\
+                                        color: rgb(255, 255, 127);')
+        self.lbl_screen.setAlignment(QtCore.Qt.AlignCenter)
+        self.lbl_screen.setText("Tela")
+        
+        self.lbl_icon = MovebleLabel(u"", self.ui.widget_icon)
+        self.lbl_icon.resize(30,30)
+        self.lbl_icon.move(10,10)
+        self.lbl_icon.setStyleSheet('background-color: rgb(255, 255, 0);\
+                                            border-color: rgb(255, 255, 255);\
+                                            font: 75 8pt "Ubuntu";\
+                                            color: rgb(0, 0, 0);')
+        self.lbl_icon.setEnabled(False)
+        
+        self.ui.radio_bl.toggled.connect(self.selected_bl)
+        self.ui.radio_br.toggled.connect(self.selected_br)
+        self.ui.radio_tr.toggled.connect(self.selected_tr)
+        self.ui.radio_tl.toggled.connect(self.selected_tl)
+        self.ui.radio_free_position.toggled.connect(self.selected_free_position)
+        
+        
+        
+    @QtCore.pyqtSlot()
+    def choose_icon(self):
+        path = QtGui.QFileDialog.getOpenFileName(self, 
+                                                 u'Selecione uma imagem',
+                                                 HOME_DIRECTORY,
+                                                 model.CONTENT_TYPES[model.Media.IMAGE])
+        if path == None:
+            return
+        
+        self.icon_path = path
+        self.ui.radio_personalized.setIcon(QtGui.QIcon(path))
+        self.ui.radio_personalized.setText("...")
+    
+    @QtCore.pyqtSlot(bool)
+    def personalized_choosed(self, checked):
+        if checked:
+            self.ui.btn_choose_icon.setEnabled(True)
+        else:
+            self.ui.btn_choose_icon.setEnabled(False)
+            
+        
+def main():
+    import sys
+    app = QtGui.QApplication(sys.argv)    
+    vp = ShowContent()
+    vp.show()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
