3b818a520c47d9e99cbb5cb6e1635fda6188c104
Caio César Viel <caioviel@gmail.com>
2013-12-05 19:02:52 -0200

    Novas interfaces

--- ui/ui_AudioPlayer.py
+++ ui/ui_AudioPlayer.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'ui/AudioPlayer.ui'
 #
-# Created: Thu Dec  5 17:41:17 2013
+# Created: Thu Dec  5 19:02:03 2013
 #      by: PyQt4 UI code generator 4.9.1
 #
 # WARNING! All changes made in this file will be lost!
