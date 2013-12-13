fcfb099c55ac195ee05e37491776671f0ebfd3da
Caio César Viel <caioviel@gmail.com>
2013-12-12 23:49:40 -0200

    teste

--- prepare_show.py
+++ prepare_show.py
@@ -47,7 +47,7 @@ def generate_html(table, broker_add, broker_port, topic):
             html_code += '"' + key + '" : ["' + value[0] + '", "' + value[1] + '"],\n'
         html_code += "};\n\n\n"
     else: 
-        html_code += "'mobile_nodes = {};\n\n\n"
+        html_code += "mobile_nodes = {};\n\n\n"
     
     html_full = header + html_code + footer
     html_name = os.path.join(HTTP_DIRECTORY, 'project', 'exibition.html')
